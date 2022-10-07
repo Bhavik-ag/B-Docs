18e9c0deae95ef45260053f0a4929a7bed2182b7
Bhavik-ag <bhavikiitian@gmail.com>
2022-10-07 16:07:48 +0530

    feat: Text Editor Setup using QuillJs

---
+++ src/components/Editor.tsx
@@ -0,0 +1,58 @@
+import React, {useState, FC} from 'react';
+import ReactQuill from 'react-quill';
+
+interface Props {
+  placeholder?: string;
+}
+
+const Editor: FC<Props> = ({placeholder}) => {
+  const [editorHtml, setEditorHtml] = useState<string>('');
+
+  console.log(editorHtml);
+
+  const formats = [
+    'header',
+    'font',
+    'size',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'indent',
+    'align',
+    'link',
+    'image',
+    'video',
+  ];
+
+  const modules = {
+    toolbar: [
+      [{header: '1'}, {header: '2'}, {font: []}],
+      [{size: []}],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{align: []}],
+      [{list: 'ordered'}, {list: 'bullet'}, {indent: '-1'}, {indent: '+1'}],
+      ['link', 'image', 'video'],
+      ['clean'],
+    ],
+    clipboard: {
+      // toggle to add extra line breaks when pasting HTML:
+      matchVisual: false,
+    },
+  };
+
+  return (
+    <ReactQuill
+      onChange={setEditorHtml}
+      modules={modules}
+      formats={formats}
+      bounds={'.app'}
+      placeholder={placeholder}
+    />
+  );
+};
+
+export default Editor;
--- src/components/document.tsx
+++ src/components/document.tsx
@@ -1,33 +1,20 @@
 import Title from './title';
-import {
-  MdFormatAlignCenter,
-  MdFormatAlignJustify,
-  MdFormatAlignLeft,
-  MdFormatAlignRight,
-  MdFormatBold,
-  MdFormatItalic,
-  MdFormatUnderlined,
-} from 'react-icons/md';
+import Editor from './Editor';
+import 'react-quill/dist/quill.snow.css';
+
 import {BsArrowLeftShort} from 'react-icons/bs';
 
-const Document = () => (
-  <div className="document-container">
-    <div className="title-container">
-      <BsArrowLeftShort className="back-to-home" />
-      <Title docTitle="Untitiled Document" />
-    </div>
-    <div className="toolbar">
-      <MdFormatBold className="toolbar-icons" />
-      <MdFormatItalic className="toolbar-icons" />
-      <MdFormatUnderlined className="toolbar-icons" />
-      <MdFormatAlignLeft className="toolbar-icons" />
-      <MdFormatAlignCenter className="toolbar-icons" />
-      <MdFormatAlignRight className="toolbar-icons" />
-      <MdFormatAlignJustify className="toolbar-icons" />
-    </div>
+const Document = () => {
+  return (
+    <div className="document-container">
+      <div className="title-container">
+        <BsArrowLeftShort className="back-to-home" />
+        <Title docTitle="Untitiled Document" />
+      </div>
 
-    <textarea className="writing-area" name="doc" id="doc-text"></textarea>
-  </div>
-);
+      <Editor placeholder="Start writing here...." />
+    </div>
+  );
+};
 
 export default Document;
